7a6465b2b6360b1c14c10cbe0f595ad5059bb373
bag3318 <bglatmman@outlook.com>
2017-10-22 18:19:42 -0700

    initiate code base

---
+++ scripts/rps.js
@@ -0,0 +1,123 @@
+var rps = (function() {
+
+    var playerScore = 0;
+    var computerScore = 0;
+
+    var gestures = ["rock", "paper", "scissors"];
+
+    var welcomeMsg = "Welcome the good ol' fashion game of Rock, Paper, Scissors! Remember the rules are simple!\nRock Beats Scissors\nScissors Beats Paper\nPaper Beats Rock";
+
+    var rules = {
+        "rock": "scissors",
+        "paper": "rock",
+        "scissors": "paper"
+    };
+
+    function welcomeMessage(msg) {
+        var ready = confirm(msg);
+        if (ready == true) {
+          playGame(3);
+        }
+        else {
+          alert("sorry, maybe next time.");
+        }
+    }
+
+    function playGame(numOfRounds) {
+        do {
+            var player = playerGuess();
+            var computer = computerGuess();
+            var result = compareGuesses(player, computer, 1);
+            if (result !== 0) {
+                numOfRounds--;
+            }
+        } while (numOfRounds > 0 && (playerScore < 3 || computerScore < 3));
+
+        var winMsg = "The player has " + playerScore + " points compared to the computer's " + computerScore + " points. So the player wins!";
+        var loseMsg = "The computer has " + computerScore + " points compared to the player's " + playerScore + " points. So the computer wins!";
+        var tieMsg = "It's a tie!";
+
+        if (playerScore > computerScore) {
+            alert(winMsg);
+        } else if (playerScore < computerScore) {
+          alert(loseMsg);
+        } else {
+          alert(tieMsg);
+        }
+
+        clearScores();
+
+    }
+
+    function playerGuess() {
+        var playerChoice = prompt("Choose rock, paper, or scissors.");
+        if (gestures.indexOf(playerChoice.toLowerCase()) >= 0) {
+              return playerChoice;
+        } else {
+          alert("You typed something else or did not spell your choice correctly please try again!");
+          return playerGuess();
+        }
+    }
+
+    function computerGuess() {
+
+        var choice = Math.random();
+
+        switch (true) {
+          case choice >= 0 && choice <= 0.33:
+            return gestures[0];
+            break;
+          case choice <= 0.66 && choice > 0.33:
+            return gestures[1];
+            break;
+          case choice > 0.66 && choice <= 1:
+            return gestures[2]
+            break;
+        }
+    }
+
+    function compareGuesses(guess1, guess2, points) {
+        //Create an alert message detailing the results
+        var output = "Player chose: " + guess1.toLowerCase() + " and the computer chose: " + guess2.toLowerCase() + "! ";
+
+        switch (true) {
+          case guess1 === guess2:
+            playerScore += 0;
+            computerScore += 0;
+            alert(output + "You and the computer guessed the same thing! Go again, no score added! " + "Player Score: " + playerScore + ", Computer Score: " + computerScore);
+            return 0;
+            break;
+          case rules[guess1] == guess2:
+            playerScore += points;
+            alert(output + "Player wins the round! " + "Player Score: " + playerScore + ", Computer Score: " + computerScore);
+            return 1;
+            break;
+          case rules[guess2] == guess1:
+            computerScore += points;
+            alert(output + "Computer wins the round! " + "Player Score: " + playerScore + ", Computer Score: " + computerScore);
+            return 2;
+            break;
+        }
+
+    }
+
+    function clearScores() {
+        playerScore = 0;
+        computerScore = 0;
+    }
+
+    function init() {
+        var element = window;
+        element.addEventListener("load", function() {
+            var masterScript = rps();
+            masterScript.startMsg(welcomeMsg);
+        });
+    }
+    return function returnAPI(api) {
+        return {
+            initiate: init,
+            startMsg: welcomeMessage
+            // playRPS: playGame
+        };
+    };
+}());
