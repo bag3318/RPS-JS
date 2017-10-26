1e91e986b4a5d4da5d56679a5fb4b6c0100f730f
bag3318 <bglatmman@outlook.com>
2017-10-25 18:02:08 -0700

    Update

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -19,13 +19,14 @@ var rps = (function() { // define a master function variable named `rps`
     console.log("gestures:", gestures);
     console.log("rules:", rules);
 
-    // define booleans for winner 
+    // define booleans for winner
     var isPlayerWinner = false;
     var isComputerWinner = false;
 
     var welcomeMsg = "Welcome to the \"Rock, Paper, Scissors\" game!\n\nRemember the rules are simple!\n\nRock Beats Scissors\nScissors Beats Paper\nPaper Beats Rock\n\nClick cancel if you don\'t wanna play.";
 
     function startGame(msg) {
+        console.log("in startGame");
         // ask the use if they want to play
         var ready = confirm(msg);
         if (ready == true) { // if player chooses "OK"
@@ -75,7 +76,7 @@ var rps = (function() { // define a master function variable named `rps`
     function playerGuess() {
         var playerChoice = prompt("Choose rock, paper, or scissors.");
         // if player's choice is in the gestures array
-        if (gestures.indexOf(playerChoice.toLowerCase()) >= 0) { 
+        if (gestures.indexOf(playerChoice.toLowerCase()) >= 0) {
             return playerChoice;
         } else {
             alert("You typed something else or did not spell your choice correctly please try again!");
@@ -85,23 +86,23 @@ var rps = (function() { // define a master function variable named `rps`
 
     function computerGuess() {
         // make 3 numbers for computer to randomly use (#'s are whole integers: 0, 1, & 2)
-        var cpuChoice = Math.floor(Math.random() * 3);
+        var cpuChoice = Math.floor((Math.random() * 3) + 1);
 
         switch (cpuChoice) {
-            case 0:
+            case 1:
                 return gestures[0];
                 break;
-            case 1:
+            case 2:
                 return gestures[1];
                 break;
-            case 2:
+            case 3:
                 return gestures[2];
                 break;
         }
     }
 
     function compareGuesses(guess1, guess2, points) {
-        var output = "Player chose: " + guess1.toLowerCase() + " and the computer chose: " + guess2.toLowerCase() + "! \n";
+        var output = "Player chose: " + guess1.toLowerCase() + ", and the computer chose: " + guess2.toLowerCase() + "! \n";
         switch (true) {
             case guess1.toLowerCase() === guess2.toLowerCase(): // if tie
                 // don't do anything with player & computer scores
@@ -113,7 +114,7 @@ var rps = (function() { // define a master function variable named `rps`
             case rules[guess1.toLowerCase()] == guess2.toLowerCase(): // if user wins
                 playerScore += points; // add 1 point to the player (specified in the `playGames` function)
                 alert(output + "\nPlayer wins the round! \n\n" + "Player Score: " + playerScore + ", Computer Score: " + computerScore);
-                determineWinner(); // call `determineWinner` function 
+                determineWinner(); // call `determineWinner` function
                 return 1;
                 break;
             case rules[guess2.toLowerCase()] == guess1.toLowerCase(): // if computer wins
@@ -126,13 +127,13 @@ var rps = (function() { // define a master function variable named `rps`
     }
 
     function setup(rpsMsg) {
-        
+
         /*
          * In this function we take a string (defined in the `init` function) and make it into an array list.
          * We do this by splitting the string at each space.
          * Then we push an `!` at the end of the array to avoid using a regex while splitting
          */
-        
+
         var rpsStr = rpsMsg;
         var rpsArray = rpsStr.split(" ");
         rpsArray.push("!");
@@ -149,29 +150,29 @@ var rps = (function() { // define a master function variable named `rps`
          * Then we need to add them to the `btnHTML` string,
          *  which will then be inserted into the `button` element with the `btn` ID.
          */
-        
+
         var i;
         for (i = 0; i < rpsArray.length; i++) {
             switch (true) {
                  // since we don't want to add a space in between `scissors` and `!`, we do this
                  // and after `!`, we do this:
                 case rpsArray[i] !== rpsArray[3] && rpsArray[i] !== rpsArray[4]:
-                    btnHTML += rpsArray[i] + " "; 
+                    btnHTML += rpsArray[i] + " ";
                     break;
                 default: // otherwise, the default option will not add a space
                     btnHTML += rpsArray[i];
-                    break; 
+                    break;
             }
         }
 
-        /*        
+        /*
          * here we use the plugin `BetterInnerHTML` to insert HTML into the button element with the `btn` ID
-         * documentation page: 
+         * documentation page:
          * http://www.optimalworks.net/blog/2007/web-development/javascript/innerhtml-alternative
          * download page:
          * http://www.optimalworks.net/resources/betterinnerhtml/
          */
-        
+
         BetterInnerHTML(btn, btnHTML, true);
 
     }
