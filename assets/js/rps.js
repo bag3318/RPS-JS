601975bbe73342809b2db1f58990593a43e8c9f8
bag3318 <bglatmman@outlook.com>
2017-10-29 12:11:44 -0700

    fix

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -142,7 +142,7 @@ var rps = (function() { // define a master function variable named `rps`
     function setup(h5Msg) {
       var h5 = document.querySelector(".h5");
       var span = document.querySelector("#header5Contents");
-      var regex = new RegExp(/\-|\+|\=|\_|\s/g, "i");
+      var regex = new RegExp(/\-|\+|\=|\_|\d/g, "i");
       var h5Array = h5Msg.split(regex);
       h5Array.push("!");
       var h5Txt = "";
@@ -171,7 +171,7 @@ var rps = (function() { // define a master function variable named `rps`
         // this is our init function that will tell the what to do on each action (load and click)
         var rpsScript = rps();
         window.addEventListener("load", function() {
-          rpsScript.load("Click-the+button=below_to play");
+          rpsScript.load("Click-the+button=below_to8play");
         }, false);
         var element = document.querySelector("#btn");
         element.addEventListener("click", function() {
