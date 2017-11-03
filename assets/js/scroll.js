828c495ff9415b8ec78eea25897fd89946b104fa
bag3318 <bglatmman@outlook.com>
2017-11-03 12:49:27 -0700

    fix

--- assets/js/scroll.js
+++
@@ -1,8 +0,0 @@
-<!--
-var scroll = (function titleScroller(text) {
-    document.title = text;
-    setTimeout(function () {
-        titleScroller(text.substr(1) + text.substr(0, 1));
-    }, 500);
-}("Rock Paper Scissors..."));
-//-->
