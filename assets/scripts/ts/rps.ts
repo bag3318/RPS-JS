8332302a5bf20b3381a8631a030bf6c10ae8221b
bag3318 <bglatmman@outlook.com>
2017-11-05 08:06:36 -0800

    fix

--- assets/scripts/ts/rps.ts
+++ assets/scripts/ts/rps.ts
@@ -1,4 +1,10 @@
-const $info: any = {
+interface GameInfo {
+    game_name: string,
+    version: number,
+    creator: string,
+    status_ok: boolean
+}
+const $info: GameInfo = {
   game_name: "RPS",
   version: 1.5,
   creator: "bag3318",
