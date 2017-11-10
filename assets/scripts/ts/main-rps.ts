1f097ccb7bd68adb46bfa000ef0df8edab8c6aa6
bag3318 <bglatmman@outlook.com>
2017-11-09 17:41:23 -0800

    Fix

--- assets/scripts/ts/main-rps.ts
+++ assets/scripts/ts/main-rps.ts
@@ -90,7 +90,7 @@ class RPS {
          * 1. if you click ok, it returns true
          * 2. if you click cancel, it returns false
          */
-        var ready: boolean = confirm(this.welcomeMsg);
+        var ready: boolean = confirm(trimmedWelcomeMsg);
         // in this case, we use a ternary operator to speed things up a bit
         var confirmReady: (void|any) = (ready) ? this.PlayGame(3) : alert("Sorry you don\'t wanna play, maybe next time! :)");
         return confirmReady;
