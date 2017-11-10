56d15002a9beaede5419f6117e6b729ad649debb
bag3318 <bglatmman@outlook.com>
2017-11-09 17:15:14 -0800

    Fix

--- assets/scripts/ts/trim.ts
+++ assets/scripts/ts/trim.ts
@@ -3,20 +3,20 @@ function trimIndentSpaces(multiLineStr: string): string {
 
   // create regex constants (vars)
   // this regex identifies the following line breaks: CRLF (Windows/DOS), LR (Linux/Unix), CR (Mac/Unix)
-  const $NEWLINES: RegExp = /(?:\n\r|\n|\r)/;
+  const NEWLINES: RegExp = /(?:\n\r|\n|\r)/;
   // this regex identifes whitespace from the beginning of the line and matches 1 or more of the preceding whitepsace token
-  const $WHITESPACE: RegExp = /(?:^\s+)/g; // uses the global flag
+  const WHITESPACE: RegExp = /(?:^\s+)/g; // uses the global flag
 
-  var splitRegEx: RegExp = RegExp($NEWLINES);
+  var splitRegEx: RegExp = RegExp(NEWLINES);
   // Split on newlines.
   let lines: string[] = multiLineStr.split(splitRegEx);
 
   // Rip out the leading whitespace.
-  var trimRegEx: RegExp = RegExp($WHITESPACE);
+  var trimRegEx: RegExp = RegExp(WHITESPACE);
   return lines.map((line) => {
     return line.replace(trimRegEx, '');
   }).join('\n').trim(); // Join back together with new line.
-  
+
 }
 
 /**
