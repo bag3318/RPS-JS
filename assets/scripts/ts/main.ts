866ff44c3c087b596a21194d6ea708db90f72d73
bag3318 <bglatmman@outlook.com>
2017-11-11 12:30:56 -0800

    fix

--- assets/scripts/ts/main.ts
+++ assets/scripts/ts/main.ts
@@ -79,7 +79,7 @@ class RPS {
         var trimmedWelcomeMsg: string = trimIndentSpaces(this.welcomeMsg);
         var ready: boolean = confirm(trimmedWelcomeMsg);
         // in this case, we use a ternary operator to speed things up a bit
-        var confirmReady: (void|any) = (ready) ? this.PlayGame(3) : alert("Sorry you don\'t wanna play, maybe next time! :)");
+        var confirmReady: void = (ready) ? this.PlayGame(3) : alert("Sorry you don\'t wanna play, maybe next time! :)");
         return confirmReady;
     }
 
