19b7e5619c54628fe0db289345399f78fa899e0f
bag3318 <bglatmman@outlook.com>
2017-11-10 16:07:24 -0800

    fix

--- assets/scripts/ts/main-rps.ts
+++ assets/scripts/ts/main-rps.ts
@@ -83,13 +83,14 @@ class RPS {
     private isComputerWinner: boolean = false;
 
     private Start(): void {
-        var trimmedWelcomeMsg: string = trimIndentSpaces(this.welcomeMsg);
         /**
          * confirm is a boolean because it has 2 return statements:
          * 1. if you click ok, it returns true
          * 2. if you click cancel, it returns false
          */
+        var trimmedWelcomeMsg: string = trimIndentSpaces(this.welcomeMsg);        
         var ready: boolean = confirm(trimmedWelcomeMsg);
+        
         // in this case, we use a ternary operator to speed things up a bit
         var confirmReady: (void|any) = (ready) ? this.PlayGame(3) : alert("Sorry you don\'t wanna play, maybe next time! :)");
         return confirmReady;
