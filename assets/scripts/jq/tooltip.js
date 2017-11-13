c12c3cc692a57b9f05a0eb9ce6618c6f9921bf65
bag3318 <bglatmman@outlook.com>
2017-11-13 14:47:44 -0800

    fix

--- assets/scripts/jq/tooltip.js
+++ assets/scripts/jq/tooltip.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
     var $btn;
     $btn = $("#btn");
-    $btn.tooltip({
-        content: "Click Me!",
-        track: true,
-        show: {
-            effect: "slideDown",
-            delay: 100
-        },
-        hide: {
-            effect: "slideUp",
-            delay: 100
-        },
-        // classes: {
-        //     "ui-tooltip": "highlight"
-        // }
+    $(function() {
+        $btn.tooltip({
+            content: "Click Me!",
+            track: true,
+            show: {
+                effect: "slideDown",
+                delay: 100
+            },
+            hide: {
+                effect: "slideUp",
+                delay: 100
+            },
+            // classes: {
+            //     "ui-tooltip": "highlight"
+            // }
+        });
     });
 });
