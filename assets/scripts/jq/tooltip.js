03385589283b30b2f832981840c88ccefb832ce3
bag3318 <bglatmman@outlook.com>
2017-11-13 14:51:59 -0800

    fix

--- assets/scripts/jq/tooltip.js
+++ assets/scripts/jq/tooltip.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     var $btn;
     $btn = $("#btn");
     $(function() {
+        $btn.attr("title", "");        
         $btn.tooltip({
             content: "Click Me!",
             track: true,
