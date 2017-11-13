5bf5142f166b4c7e8d408b3f4a24ec51355b8fda
bag3318 <bglatmman@outlook.com>
2017-11-13 07:40:51 -0800

    Fix

--- assets/scripts/ts/ref/interfaces.ts
+++ assets/scripts/ts/ref/interfaces.ts
@@ -1,11 +1,11 @@
-interface Rules {
-  [index: string]: string;
-}
 interface GameInfo {
   game_name: string;
   version: number;
   creator: string;
   status_ok: boolean;
+}
+interface Rules {
+  [index: string]: string;
 }
 /**
  * interface Gestures {
