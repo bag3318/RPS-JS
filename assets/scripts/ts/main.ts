67d5dce5c06a82097d16ca58d557d5fe8c222b10
bag3318 <bglatmman@outlook.com>
2017-11-13 16:52:28 -0800

    fix

--- assets/scripts/ts/main.ts
+++ assets/scripts/ts/main.ts
@@ -140,7 +140,7 @@ class RPS {
                 return this.gestures[2];
             }
             default: {
-                throw new Error("Error, cpu choice not valid.");
+                return ""; // we need to return a blank string to fullfill the the return type of this function
             }
         }
     }
