5bff5a530eccd31db539ba1da48eae9f90e2c993
bag3318 <bglatmman@outlook.com>
2017-11-13 18:48:28 -0800

    fix

---
+++ assets/scripts/jq/pageLoad.js
@@ -0,0 +1,14 @@
+// intialize jQuery
+jQuery("document").ready(function(){
+    function $consoleLog($isPageLoaded) {
+        return console.log("Is page loaded:",$isPageLoaded)
+    }
+    function $checkIfPageLoadedIsComplete() {
+        if (document.readyState) {
+            $consoleLog(true);
+        } else {
+            $consoleLog(false);
+        }
+    }
+    $checkIfPageLoadedIsComplete();
+});
